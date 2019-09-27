e4197518527fb12ed4f7a7e71587cf14e2959fa7
Fabio Papa <fabtheman@gmail.com>
2019-09-27 13:41:33 -0700

    Fix linting issues

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const t1 = new Withdrawal(50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
